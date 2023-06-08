01d0c53ecdf34164f1c708081a39c9846f041ab4
VINKO-NEVESCANIN <nevescanin.vinko@gmail.com>
2023-06-08 15:25:48 -0700

    Creacion de grafica
    Puede mostrar datos pero no genera la tabla.

--- RAICEZ/Code-Raiz/OpenRaizXl.py
+++ RAICEZ/Code-Raiz/OpenRaizXl.py
@@ -1,17 +1,29 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 
 archivo_excel = r'C:\Users\VINKO\Documents\GitHub\Raicez\RAICEZ\Excel\Temperatura control 15 feb- 13 abr 2020_OFICIAL.xlsx'
 columnas_deseadas = ['TtarRC_Avg(1)', 'TtarRC_Avg(2)', 'TtarRC_Avg(3)', 'TtarRC_Avg(4)', 'TtarRC_Avg(5)', 'TtarRC_Avg(6)', 'TtarRC_Avg(7)', 'TtarRC_Avg(8)',
                      'TtarHC_Avg(1)','TtarHC_Avg(2)','TtarHC_Avg(3)','TtarHC_Avg(4)','TtarHC_Avg(5)','TtarHC_Avg(6)','TtarHC_Avg(7)','TtarHC_Avg(8)']  # Reemplaza con los nombres de tus columnas
-
+archivo_salida = r'C:\Users\VINKO\Documents\GitHub\Raicez\Temperaturas_2022.xlsx'
+#print(df.head())
 
 try:
     df = pd.read_excel(archivo_excel, engine='openpyxl')
+    df_seleccionado = df[columnas_deseadas]
+    df_seleccionado.to_excel(archivo_salida, index=False)
+    print('Datos guardados exitosamente en el archivo:', archivo_salida)
+    print(df_seleccionado)
     # Realiza las operaciones necesarias con el DataFrame
     # ...
-    print(df.head())
-    df_seleccionado = df[columnas_deseadas]
-    print(df_seleccionado)
+    
+    # Crear gráfica de líneas
+    df_seleccionado.plot(x='TtarRC_Avg(1)', y=['TtarRC_Avg(2)', 'TtarRC_Avg(3)'], kind='line')
+    plt.xlabel('TtarRC_Avg(1)')
+    plt.ylabel('Valores')
+    plt.title('Gráfica de Líneas')
+    plt.legend()
+    plt.show()
+
 except FileNotFoundError:
     print('El archivo no existe o la ruta es incorrecta.')
 except Exception as e:
