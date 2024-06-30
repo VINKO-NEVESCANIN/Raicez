f397cd46246ffec8329fd8b830376b566f88f7ce
VINKO-NEVESCANIN <nevescanin.vinko@gmail.com>
2024-06-30 02:32:38 -0700

    Cambios de Entrada de Datos.
    La entrada de datos de la automatización.

--- RAICEZ/Alerta/Automatizacion.py
+++ RAICEZ/Alerta/Automatizacion.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog, messagebox, simpledialog
+from tkinter import filedialog, messagebox, simpledialog, ttk
 import pandas as pd
 import os
 from openpyxl import load_workbook
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import schedule
 import time
 from threading import Thread
+from datetime import datetime
 
 epsilon = 1.001  # Valor pequeño para ajustar los límites
 
@@ -16,12 +17,20 @@ archivo_global = None
 columnas_seleccionadas_global = None
 valor_minimo_global = None
 valor_maximo_global = None
+ruta_guardado_global = None
 
 def cargar_archivo():
     ruta_archivo = filedialog.askopenfilename(filetypes=[('Archivos Excel', '*.xlsx')])
     if ruta_archivo:
         entry_ruta_archivo.delete(0, tk.END)
         entry_ruta_archivo.insert(tk.END, ruta_archivo)
+
+def seleccionar_guardado():
+    global ruta_guardado_global
+    ruta_guardado_global = filedialog.askdirectory()
+    if ruta_guardado_global:
+        entry_ruta_guardado.delete(0, tk.END)
+        entry_ruta_guardado.insert(tk.END, ruta_guardado_global)
 
 def procesar_datos():
     global archivo_global, columnas_seleccionadas_global, valor_minimo_global, valor_maximo_global
@@ -39,6 +48,7 @@ def procesar_datos():
                     columnas_seleccionadas_global = columnas_seleccionadas
                     valor_minimo_global = valor_minimo
                     valor_maximo_global = valor_maximo
+                    seleccionar_guardado()
                     filtrar_datos(archivo, columnas_seleccionadas, valor_minimo, valor_maximo)
                 except (TypeError, ValueError):
                     messagebox.showerror("Error", "Valor mínimo o máximo no válido. Operación cancelada.")
@@ -46,6 +56,7 @@ def procesar_datos():
                 try:
                     archivo_global = archivo
                     columnas_seleccionadas_global = columnas_seleccionadas
+                    seleccionar_guardado()
                     filtrar_datos(archivo, columnas_seleccionadas)
                 except (TypeError, ValueError):
                     messagebox.showerror("Error", "Valores no válidos. Operación cancelada.")
@@ -101,7 +112,7 @@ def filtrar_datos(archivo, columnas_seleccionadas, valor_minimo=None, valor_maxi
         return
 
     nombre_archivo = os.path.splitext(os.path.basename(archivo))[0]
-    archivo_salida = f'{nombre_archivo}_datos_filtrados.xlsx'
+    archivo_salida = os.path.join(ruta_guardado_global, f'{nombre_archivo}_datos_filtrados.xlsx')
     df.to_excel(archivo_salida, index=False)
 
     wb = load_workbook(archivo_salida)
@@ -157,19 +168,73 @@ def tarea_automatica():
         except Exception as e:
             print(f"Error en la tarea automática: {e}")
 
-def iniciar_automatizacion():
-    # Programar la tarea para que se ejecute cada día a una hora específica (por ejemplo, a las 8:00 AM)
-    schedule.every().day.at("08:00").do(tarea_automatica)
+def configurar_automatizacion():
+    # Crear una nueva ventana para la configuración de la automatización
+    ventana_autom = tk.Toplevel()
+    ventana_autom.title('Configurar Automatización')
+
+    dias_vars = {}
+    dias = ['lunes', 'martes', 'miércoles', 'jueves', 'viernes', 'sábado', 'domingo']
     
-    def run_scheduler():
-        while True:
-            schedule.run_pending()
-            time.sleep(1)
+    for i, dia in enumerate(dias):
+        var = tk.BooleanVar()
+        chk = tk.Checkbutton(ventana_autom, text=dia.capitalize(), variable=var)
+        chk.grid(row=i, column=0, sticky='w')
+        dias_vars[dia] = var
     
-    # Ejecutar el programador en un hilo separado
-    t = Thread(target=run_scheduler)
-    t.daemon = True
-    t.start()
+    label_hora = tk.Label(ventana_autom, text='Hora (HH:MM, 24h):')
+    label_hora.grid(row=7, column=0)
+    entry_hora = tk.Entry(ventana_autom)
+    entry_hora.grid(row=7, column=1)
+    
+    def guardar_configuracion():
+        dias_seleccionados = [dia for dia, var in dias_vars.items() if var.get()]
+        hora = entry_hora.get()
+        
+        try:
+            datetime.strptime(hora, '%H:%M')  # Validar el formato de la hora
+            if dias_seleccionados:
+                configurar_schedule(dias_seleccionados, hora)
+                ventana_autom.destroy()
+            else:
+                messagebox.showerror("Error", "Seleccione al menos un día para la automatización.")
+        except ValueError:
+            messagebox.showerror("Error", "Hora no válida. Use el formato HH:MM.")
+    
+    button_guardar = tk.Button(ventana_autom, text='Guardar', command=guardar_configuracion)
+    button_guardar.grid(row=8, column=0, columnspan=2)
+    
+def configurar_schedule(dias, hora):
+    if ruta_guardado_global:
+        try:
+            def tarea_automatica():
+                if archivo_global and columnas_seleccionadas_global:
+                    try:
+                        if valor_minimo_global is not None and valor_maximo_global is not None:
+                            filtrar_datos(archivo_global, columnas_seleccionadas_global, valor_minimo_global, valor_maximo_global)
+                        else:
+                            filtrar_datos(archivo_global, columnas_seleccionadas_global)
+                    except Exception as e:
+                        print(f"Error en la tarea automática: {e}")
+
+            for dia in dias:
+                dia_schedule = getattr(schedule.every(), dia)
+                dia_schedule.at(hora).do(tarea_automatica)
+            
+            messagebox.showinfo("Automatización", "La automatización ha sido configurada.")
+            
+            # Iniciar el hilo para ejecutar `schedule`
+            def run_scheduler():
+                while True:
+                    schedule.run_pending()
+                    time.sleep(1)
+            
+            scheduler_thread = Thread(target=run_scheduler, daemon=True)
+            scheduler_thread.start()
+        except Exception as e:
+            messagebox.showerror("Error", f"Error al iniciar la automatización: {e}")
+    else:
+        messagebox.showerror("Error", "Seleccione una carpeta para guardar los archivos antes de iniciar la automatización.")
 
 ventana = tk.Tk()
 ventana.title('Selección de datos de Excel')
@@ -186,10 +251,17 @@ label_columnas.grid(row=1, column=0)
 entry_columnas = tk.Entry(ventana, width=40)
 entry_columnas.grid(row=1, column=1, columnspan=2)
 
-button_procesar = tk.Button(ventana, text='Procesar', command=procesar_datos)
-button_procesar.grid(row=2, column=0, columnspan=3)
+label_ruta_guardado = tk.Label(ventana, text='Carpeta para guardar:')
+label_ruta_guardado.grid(row=2, column=0)
+entry_ruta_guardado = tk.Entry(ventana, width=40)
+entry_ruta_guardado.grid(row=2, column=1)
+button_seleccionar_guardado = tk.Button(ventana, text='Seleccionar', command=seleccionar_guardado)
+button_seleccionar_guardado.grid(row=2, column=2)
 
-button_automatizar = tk.Button(ventana, text='Iniciar Automatización', command=iniciar_automatizacion)
-button_automatizar.grid(row=3, column=0, columnspan=3)
+button_procesar = tk.Button(ventana, text='Procesar', command=procesar_datos)
+button_procesar.grid(row=3, column=0, columnspan=3)
+
+button_automatizar = tk.Button(ventana, text='Configurar Automatización', command=configurar_automatizacion)
+button_automatizar.grid(row=4, column=0, columnspan=3)
 
 ventana.mainloop()
